1da9535df6294f4b352360dd36b8f52d0ff0a4ec
federicofusco <40967674+federicofusco@users.noreply.github.com>
2023-05-19 22:45:06 +0200

    Updated /api/upload/data

--- interface/dashboard/pages/api/upload/data.ts
+++ interface/dashboard/pages/api/upload/data.ts
@@ -29,19 +29,13 @@ interface UploadResponse {
         data?: any
 }
 
-async function upload (device_id: string, data: UploadRequest, fn: Function, response: NextApiResponse<UploadResponse>) {
+async function upload (data: UploadRequest, fn: Function, response: NextApiResponse<UploadResponse>) {
         
         // Verifies the ID token
         const { auth, firestore } = getAdmin ();
         auth
                 .verifyIdToken ( data.jwt )
                 .then ( async user => {
-
-                        // Checks that the device ID corresponds to the JWT
-                        if (device_id !== user.uid) {
-                                response.status ( 401 ).json ({ message: "JWT doesn't match the device's ID!", code: "INVALID_JWT" });
-                                return;
-                        }
 
                         // Attempts to fetch the device by their ID
                         const device_ref = firestore
@@ -83,13 +77,9 @@ async function upload (device_id: string, data: UploadRequest, fn: Function, res
 
 export default async function handler ( request: NextApiRequest, response: NextApiResponse<UploadResponse> ) {
 
-        // Gets the device ID
-        const { device_id } = request.query;
-        if ( !device_id || device_id.length === 0 || Array.isArray ( device_id ) ) return response.status ( 400 ).json ({ message: "Missing or invalid device ID!", code: "INVALID_DEVICE_ID" });
-
         // Gets the user's credentials
         const data: UploadRequest = request.body;
-        if ( !data.jwt ) return response.status ( 401 ).json ({ message: "Missing JWT token!", code: "MISSING_TOKEN" });
+        if ( !data.jwt  || Array.isArray ( data.jwt ) ) return response.status ( 401 ).json ({ message: "Missing JWT token!", code: "MISSING_TOKEN" });
 
-        await upload ( device_id, data, cors, response );
+        await upload ( data, cors, response );
 }
