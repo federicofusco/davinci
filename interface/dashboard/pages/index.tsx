604496188448e35aa41959058da6041df3fa6a9d
federicofusco <40967674+federicofusco@users.noreply.github.com>
2023-05-26 14:39:43 +0200

    Fixed some more bugs

--- interface/dashboard/pages/index.tsx
+++ interface/dashboard/pages/index.tsx
@@ -1,10 +1,16 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home = () => {
         const router = useRouter ();
-        router.push ("/auth/login");
         
-        return null;
+        useEffect (() => {
+                router.push ( "/auth/login" );
+        }, []);
+        
+        return <div className="w-screen h-screen flex justify-center">
+                <p className="text-center w-full text-white my-auto">Redirecting. Please wait...</p>
+        </div>;
 }
 
 export default Home;
